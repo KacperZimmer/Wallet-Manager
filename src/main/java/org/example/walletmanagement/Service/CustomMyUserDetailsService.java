6a15037e245922dec9cec40015f7c1988497f18d
Kacper <kacperzimmer@outlook.com>
2024-11-01 00:26:57 +0100

    part of registering

--- src/main/java/org/example/walletmanagement/Service/CustomMyUserDetailsService.java
+++ src/main/java/org/example/walletmanagement/Service/CustomMyUserDetailsService.java
@@ -1,4 +1,30 @@
 package org.example.walletmanagement.Service;
 
-public class CustomMyUserDetailsService {
+import org.example.walletmanagement.Entity.User;
+import org.example.walletmanagement.Repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+import java.util.ArrayList;
+
+public class CustomMyUserDetailsService implements UserDetailsService {
+
+    UserService userService;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+
+        if(this.userService.getUserByUsername(username) == null){
+            throw new UsernameNotFoundException(username);
+        }
+        User user = userService.getUserByUsername(username);
+
+        return null;
+
+    }
+    CustomMyUserDetailsService(UserService userService) {
+        this.userService = userService;
+    }
 }
