fc22744814d7c806a709b88c7e7a1f01ea2f7ee9
Kacper <kacperzimmer@outlook.com>
2024-11-08 20:13:30 +0100

    frontend improvements

--- src/main/java/org/example/walletmanagement/Controller/MainPageController.java
+++ src/main/java/org/example/walletmanagement/Controller/MainPageController.java
@@ -3,7 +3,10 @@ package org.example.walletmanagement.Controller;
 import org.example.walletmanagement.Service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.security.Principal;
 
 @Controller
 public class MainPageController {
@@ -11,7 +14,12 @@ public class MainPageController {
 
 
     @GetMapping("/")
-    public String homePage() {
+    public String homePage(Principal principal, Model model) {
+
+        boolean isLoggenIn = (principal != null);
+
+        model.addAttribute("isLoggedIn", isLoggenIn);
+
 
         return "HomePage/homePage";
     }
