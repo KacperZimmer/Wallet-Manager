61e8d340671c277062b157d71b850667002dc05c
Kacper <kacperzimmer@outlook.com>
2024-11-16 14:29:16 +0100

    modal window for adding expenses

--- src/main/java/org/example/walletmanagement/Component/DataInitializer.java
+++ src/main/java/org/example/walletmanagement/Component/DataInitializer.java
@@ -35,12 +35,7 @@ public class DataInitializer implements CommandLineRunner {
         this.roleService.saveRole(roleUser);
         this.roleService.saveRole(roleAdmin);
 
-        User user = new User();
-        user.setUsername("admin");
-        user.setPassword("$2a$12$Zd.WxxJl63MMsUjLlNbRIe71DyU9jn/nNBs6EOy0y2J/sIgUhQv62");
-        user.setEmail("admin@admin.com");
-        user.getRoles().add(roleAdmin);
-        this.userService.saveUser(user);
+
 
 
     }
